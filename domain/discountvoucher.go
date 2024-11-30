74f8d9dc9dbeb3841d2b8d8bebfe15d6586aa426
Paulus Otto Harman <paulus.otto.harman@gmail.com>
2024-12-01 03:48:07 +0700

    feat:create voucher

--- domain/discountvoucher.go
+++ domain/discountvoucher.go
@@ -1,9 +1,9 @@
 package domain
 
 type DiscountVoucher struct {
-	ID        uint    `gorm:"primaryKey;autoIncrement" json:"-"`
-	VoucherID uint    `json:"-"`
-	Rate      float32 `gorm:"precision:5;scale:2" json:"rate"`
-	Amount    uint32  `gorm:"precision:4" json:"amount"`
+	ID        uint     `gorm:"primaryKey;autoIncrement" json:"-"`
+	VoucherID uint     `json:"-"`
+	Rate      *float32 `gorm:"precision:5;scale:2" json:"rate"`
+	Amount    *uint32  `gorm:"precision:4" json:"amount"`
 	Condition `gorm:"embedded" json:"terms_and_condition"`
 }
